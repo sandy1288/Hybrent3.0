796a381681881ea0b8d9f4018638efb8e5f75ae0
sandy1288 <sandy.hybrent@gmail.com>
2019-11-25 18:02:00 +0530

    3.1

---
+++ Testcases/Order/order.js
@@ -0,0 +1,99 @@
+describe('Hybrent Order Module', function () {
+  var EC = protractor.ExpectedConditions;
+  var PO_Num = browser.params.itemCatalog.PO_Number;
+  var randNumber = browser.params.itemCatalog.randNumber;
+  it('Open order module', function () {
+    element(by.linkText('Orders')).click();
+    expect(browser.getTitle()).toEqual('My Orders');
+
+  });
+  it('List page should display list of filters', function () {
+    browser.sleep(2000);
+    expect(element(by.model('searchForm.search')).isPresent()).toBeTruthy();
+    // expect(element(by.model('searchForm.vendorFilter')).isPresent()).toBeTruthy();
+    expect(element(by.model('searchForm.statusFilter')).isPresent()).toBeTruthy();
+    expect(element(by.model('searchForm.typeFilter')).isPresent()).toBeTruthy();
+    expect(element(by.model('searchForm.project')).isPresent()).toBeTruthy();
+    expect(element(by.model('searchForm.departmentId')).isPresent()).toBeTruthy();
+    expect(element(by.model('$ctrl.selectedValue.name')).isPresent()).toBeTruthy();
+  });
+
+  it('Order list page search with PO# Found', function () {
+    element(by.model('searchForm.statusFilter')).click();
+    element(by.model('searchForm.search')).sendKeys(PO_Num + randNumber);
+    element(by.repeater('status in $select.items')).click();
+    element(by.buttonText('Search')).click();
+    browser.sleep(5000);
+    expect(element(by.linkText(PO_Num + randNumber)).isPresent()).toBeTruthy();
+
+  });
+
+  it('open order detail page', function () {
+    element(by.linkText('PO_Num + randNumber')).click();
+    expect(browser.getTitle()).toEqual('Purchase Order');
+  });
+
+  it('update item price on order detail page', function () {
+    browser.sleep(2000);
+    browser.executeScript("arguments[0].scrollIntoView();", element(by.className('a-link')).getWebElement()).then(function () {
+      element(by.className('a-link')).click();
+    });
+    browser.sleep(2000);
+    element(by.model('itemForm.price')).clear().sendKeys('12');
+    element(by.buttonText('Update Price')).click();
+    browser.executeScript("arguments[0].scrollIntoView(0,0);", element(by.className('pagehead')).getWebElement()).then(function () {
+      browser.sleep(2000);
+      element(by.buttonText('Save')).click();
+    })
+    browser.sleep(2000);
+    expect($('.toast-message').getText()).toEqual('Order successfully updated.');
+    expect(browser.getTitle()).toEqual('My Orders');
+  });
+
+  it('verify that all option appears in Order drop down', function () {
+    let order = element(by.repeater('order in ordersData.purchaseOrders'));
+    browser.wait(EC.elementToBeClickable(order.element(by.css('.dropdown-toggle'))), 5000);
+    order.element(by.css('.dropdown-toggle')).click();
+    // browser.actions().mouseMove(order.element(by.css('li.po-print'))).perform();
+    // browser.wait(EC.elementToBeClickable(order.element(by.css('li.po-print'))), 5000);
+    expect(element(by.css('li.po-print')).isPresent()).toBe(true);
+    expect(element(by.linkText('Download PO')).isPresent()).toBe(true);
+    expect(element(by.linkText('Print Items')).isPresent()).toBe(true);
+    expect(element(by.linkText('Invoices')).isPresent()).toBe(true);
+    expect(element(by.linkText('PO Log')).isPresent()).toBe(true);
+    expect(element(by.linkText('Notes')).isPresent()).toBe(true);
+    expect(element(by.linkText('Documents')).isPresent()).toBe(true);
+    expect(element(by.linkText('Add Items to Cart')).isPresent()).toBe(true);
+    expect(element(by.linkText('Delete PO')).isPresent()).toBe(true);
+  });
+
+
+  it('add invoice for newly created order', function () {
+    element(by.buttonText('Search')).click();
+    let order = element(by.repeater('order in ordersData.purchaseOrders'));
+    browser.wait(EC.elementToBeClickable(order.element(by.css('.dropdown-toggle'))), 5000);
+    order.element(by.css('.dropdown-toggle')).click();
+    // let order = element(by.repeater('order in ordersData.purchaseOrders'));
+    // browser.wait(EC.elementToBeClickable(order.element(by.css('.dropdown-toggle'))), 5000);
+    // order.element(by.css('.dropdown-toggle')).click();
+    browser.actions().mouseMove(order.element(by.linkText('Invoices'))).perform();
+    browser.wait(EC.elementToBeClickable(order.element(by.linkText('Add Invoice'))), 5000);
+    element(by.linkText('Add Invoice')).click();
+    expect(browser.getTitle()).toEqual('Invoice Detail');
+    element(by.model('vm.InvNo')).sendKeys(PO_Num + randNumber);
+    element(by.model('vm.InvoicePaymentTerm')).$('[label="' + 'Other' + '"]').click();
+    browser.sleep(1000);
+    element(by.model('vm.SelectedItem')).click();
+    element(by.xpath('//option[contains(text(),"All")]')).click();
+    element(by.buttonText('Add Item')).click();
+    element(by.buttonText('Save')).click();
+    browser.sleep(1000);
+    element(by.css('.sa-button-container')).element(by.buttonText('Yes')).click();
+    expect($('.toast-message').getText()).toEqual('Invoice saved successfully.');
+    browser.sleep(1000);
+    expect(browser.getTitle()).toEqual('PO Invoice');
+    browser.wait(EC.invisibilityOf($('.pg-loading-center-middle')), 20000);
+    element(by.xpath('//span[contains(text(),"Receive")]')).click();
+    expect(browser.getTitle()).toEqual('Receive PO');
+  })
+});
---
+++ Testcases/Template/Templatelist.js
@@ -0,0 +1,51 @@
+describe('Hybrent Templates Module', function () {
+  var EC = protractor.ExpectedConditions;
+  var templateName = browser.params.Templates.templateName;
+  var General_sku = browser.params.itemCatalog.General_sku;
+  var userfacility = browser.params.userfacility.facility_xpath;
+  var randNumber = browser.params.itemCatalog.randNumber;
+
+  it('Templates List page should open', function () {
+    browser.executeScript("arguments[0].scrollIntoView();", element(by.css('a > span.template')).getWebElement()).then(function () {
+      element((by.linkText('Templates'))).click();
+    });
+    expect(browser.getTitle()).toEqual('Manage Templates');
+  });
+  it('List page should display templates search filter', function () {
+    expect(element(by.model('searchForm.searchFilter')).isPresent()).toBeTruthy();
+
+  });
+  it('Add Template', function () {
+    element(by.buttonText('Add Template')).click();
+    element(by.model('tempData.template_name')).sendKeys(templateName + randNumber);
+    element(by.buttonText('Create Template')).click();
+    expect($('.toast-message').getText()).toEqual('Template created successfully.');
+    browser.wait(EC.elementToBeClickable(element(by.buttonText('Save'))), 5000);
+  });
+
+  it('Add item to newly created template', function () {
+    element(by.model('searchParams.search')).sendKeys(General_sku + randNumber);
+    browser.sleep(1000);
+    element(by.buttonText('Add')).click();
+    element(by.buttonText('Save')).click();
+    browser.sleep(1000);
+    element.all(by.repeater('item in vendor.items')).each(function (element1, index) {
+      element1.element(by.binding('item.sku')).getText().then(function (text) {
+        browser.sleep(2000);
+        expect(text).toEqual(General_sku + randNumber);
+      });
+    });
+    element(by.buttonText('Close')).click();
+  });
+
+  it('Search template by name', function () {
+    element(by.model('searchForm.searchFilter')).sendKeys(templateName + randNumber);
+    element(by.buttonText('Search')).click();
+    browser.sleep(2000);
+    element.all(by.repeater('template in templates')).getText().then(function (text) {
+      expect(text).toStartWith(templateName + randNumber);
+    });
+
+  });
+
+});
